037363001b198741f748f877c78c3a03f690c3d5
Raj Kariya <114669281+raj-kariya@users.noreply.github.com>
2024-09-16 23:21:30 +0530

    Update Example_Code_1.py

--- Example Codes used in Paper/Example_Code_1.py
+++ Example Codes used in Paper/Example_Code_1.py
@@ -1,8 +1,8 @@
+# Original version
 Equilateral = 0
 Isosceles = 1
 Scalene = 2
 Right_Triangle=3
-
 def triangle_type(a, b, c):
   if a < b + c and b < a + c and c < a + b:
     if  a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or c**2 + a**2 == b**2:
@@ -11,5 +11,19 @@ def triangle_type(a, b, c):
       return Equilateral
     elif a == b or b == c or a == c:
       return Isosceles
+    return Scalene
+  return None
+  
+# Modified Version
+Equilateral = 0
+Scalene = 1
+Right_Triangle=2
+
+def triangle_type(a, b, c):
+  if a < b + c and b < a + c and c < a + b:
+    if  a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or c**2 + a**2 == b**2:
+      return Right_Triangle
+    if a == b == c:
+      return Equilateral
     return Scalene
   return None
