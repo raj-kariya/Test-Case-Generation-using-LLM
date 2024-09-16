ed5b3ecc1434b679beb11d7058544522d88d645e
Raj Kariya <114669281+raj-kariya@users.noreply.github.com>
2024-09-16 23:24:45 +0530

    Update Example_Code_4.py

--- Example Codes used in Paper/Example_Code_4.py
+++ Example Codes used in Paper/Example_Code_4.py
@@ -1,6 +1,15 @@
+# Original Version
 def any_int(x, y, z):
     if isinstance(x, int) and isinstance(y, int) and isinstance(z, int):
         if (x + y == z) or (x + z == y) or (y + z == x):
+              return True
+        return False
+    return False
+
+# Modified Version
+def any_int(x, y, z):
+    if isinstance(x, int) and isinstance(y, int) and isinstance(z, int):
+        if (x + y == z) or (x + z != y) or (y + z == x):   # Fault introduced in the condition
               return True
         return False
     return False
